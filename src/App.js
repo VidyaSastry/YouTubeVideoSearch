07a39366a8e725f752720ed9723cf5439a76900c
Sree Vidya Sastry <sreevidya.sastry12@gmail.com>
2017-02-26 17:38:58 -0800

    Adding lodash to throttle search

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import './style/App.css';
 import SearchBar from './components/search_bar';
@@ -27,9 +28,11 @@ class App extends Component {
   };
 
   render() {
+    const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300);
+
     return (
       <div className="App">
-        <SearchBar onSearchTermChanged={term => this.videoSearch(term)}/>
+        <SearchBar onSearchTermChanged={videoSearch}/>
         <VideoDetail video={this.state.selectedVideo}/>
         <VideoList videos={this.state.videos}
         onVideoSelect={selectedVideo => this.setState({selectedVideo})}/>
